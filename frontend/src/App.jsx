ae564220e5ba1454538e8e2949be89162ec132fe
Chintan1911 <chintan191199@gmail.com>
2024-07-12 17:25:39 -0400

    Create Design for LoginPopUp
    Login and Sign Up design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,16 @@ import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Footer from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup'
 
 const App = () => {
+  const [showLogin,setShowLogin] = useState(false)
   return (
     <>
+         {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
-      <Navbar />
-      <Routes>
+    <Navbar setShowLogin={setShowLogin}/>
+    <Routes>
       <Route path='/' element={<Home />} />
       </Routes>
     </div>
